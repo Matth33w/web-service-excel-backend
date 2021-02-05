c53faa348661c04a73577fb89e6b810c60ae9b85
Matth33w <matheus.ibrahim@hotmail.com>
2021-02-05 16:54:29 -0300

    Update rotas

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,9 @@ const routes = express.Router();
 const UserController = require("./controllers/UserController");
 const XLSXController = require("./controllers/XLSXController");
 
-routes.get("/users", async (request, response) => UserController.read(request, response));
+routes.get("/users", async (request, response) => UserController.authenticate(request, response));
 routes.post("/users", async (request, response) => UserController.create(request, response));
+routes.patch("/users", async (request, response) => UserController.login(request, response));
 
 routes.post("/download-file", async (request, response) => XLSXController.GenerateFile(request, response));
 
